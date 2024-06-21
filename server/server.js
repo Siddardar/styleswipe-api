c237b820f79871fb1976c7278acd26473a7bdbc4
Sidd <sr.sanagala@outlook.com>
2024-06-21 16:25:54 +0800

    Updated Port

--- server/server.js
+++ server/server.js
@@ -95,7 +95,7 @@ app.post('/payment-sheet', async (req, res) => {
 
 
 //Run Server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
